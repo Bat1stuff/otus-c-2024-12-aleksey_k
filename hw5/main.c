2b92c61a67be76352b24c562900e57d881610bd4
A.Kravchuk <bat1stuff@pm.me>
2025-02-07 21:13:27 +0300

    fix

--- hw5/main.c
+++ hw5/main.c
@@ -57,12 +57,12 @@ void renderTreeViewWidget(GtkWidget *window)
 static void activate(GtkApplication* app, gpointer user_data)
 {
     GtkWidget *window = gtk_application_window_new(app);
-    gtk_window_set_title (GTK_WINDOW (window), "TreeView");
-    gtk_window_set_default_size (GTK_WINDOW (window), 400, 400);
+    gtk_window_set_title(GTK_WINDOW(window), "TreeView");
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
 
     renderTreeViewWidget(window);
 
-    gtk_widget_show_all (window);
+    gtk_widget_show_all(window);
 }
 
 int main(int argc, char **argv)
@@ -70,7 +70,7 @@ int main(int argc, char **argv)
     GtkApplication *app = gtk_application_new("org.example.TreeModelExample", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     const int status = g_application_run(G_APPLICATION(app), argc, argv);
-    g_object_unref (app);
+    g_object_unref(app);
 
     return status;
 }
